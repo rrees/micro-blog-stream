596b9cd2cb834b0d8b1b245fb16e802e6126fa38
Robert Rees <shudderfix@gmail.com>
2016-10-21 22:31:58 +0100

    Models a single user situation

--- models.py
+++ models.py
@@ -1,2 +1,15 @@
 from google.appengine.ext import ndb
 
+class Stream(ndb.Model):
+	name = ndb.StringProperty(required=True)
+	description = ndb.StringProperty()
+	created_datetime = ndb.DatetimeProperty(auto_now_add = True)
+	updated_datetime = ndb.DatetimeProperty(auto_now = True)
+
+class Post(ndb.Model):
+	content = ndb.TextProperty(required=True)
+	title = ndb.StringProperty()
+	tags = ndb.StringProperty(repeated=True)
+	stream_key = ndb.KeyProperty(kind=Stream)
+	created_datetime = ndb.DatetimeProperty(auto_now_add = True)
+	updated_datetime = ndb.DatetimeProperty(auto_now = True)
