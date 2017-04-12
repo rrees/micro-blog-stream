4427df72bb24b24a6783dcb90e2e9fdb27fd6348
Robert Rees <shudderfix@gmail.com>
2017-04-12 07:55:25 +0100

    Adds a private flag

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class Post(ndb.Model):
 	content = ndb.TextProperty(required=True)
 	title = ndb.StringProperty()
 	tags = ndb.StringProperty(repeated=True)
+	private = ndb.BooleanProperty(required=True, default=False)
 	user_id = ndb.StringProperty(required=True)
 	created_datetime = ndb.DateTimeProperty(auto_now_add = True)
 	updated_datetime = ndb.DateTimeProperty(auto_now = True)
