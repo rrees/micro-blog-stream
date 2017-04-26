58ff328f3ea8ded0e139a69abf5bd9332a1783de
Robert Rees <shudderfix@gmail.com>
2017-04-26 12:05:17 +0100

    Very basic Markdown generation and cleaning is now done via the filter

--- filters.py
+++ filters.py
@@ -1,6 +1,8 @@
 import markdown
 import bleach
 
+allowed_tags = bleach.ALLOWED_TAGS + ['p']
+
 def safe_html(some_markdown):
-	#return bleach.clean(markdown.markdown(some_markdown))
-	return markdown.markdown(some_markdown)
+	return bleach.clean(markdown.markdown(some_markdown), tags=allowed_tags)
+	#return markdown.markdown(some_markdown)
