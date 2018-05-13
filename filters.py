56bef88814274f0179a86ce3dfbf1b9046fa65a6
Robert Rees <shudderfix@gmail.com>
2018-05-13 10:54:46 +0100

    Changes the permitted headings to reflect the framing layout

--- filters.py
+++ filters.py
@@ -1,7 +1,7 @@
 import markdown
 import bleach
 
-allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h2', 'h3', 'br']
+allowed_tags = bleach.ALLOWED_TAGS + ['p', 'h3', 'h4', 'br']
 
 def safe_html(some_markdown):
 	return bleach.clean(markdown.markdown(some_markdown), tags=allowed_tags)
